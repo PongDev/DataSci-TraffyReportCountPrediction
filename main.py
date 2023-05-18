4aaebbb8c1c1bf379732344126b05a8f70be0e41
PongDev <pongwiwat.dev@gmail.com>
2023-05-18 11:05:45 +0700

    feat: add frontend

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+app.mount("/", StaticFiles(directory="build", html=True))
 
-@app.get("/ping")
+
+@app.get("/ping", response_class=JSONResponse)
 def index():
     return {"Ping": "Pong!"}
